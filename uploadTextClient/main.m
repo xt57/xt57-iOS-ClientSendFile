190573ee2b1eda01890b95905b0d2ff5918c6f06
Eric Walker <charles.walker@tn.gov>
2017-10-19 07:56:20 -0500

    Initial Commit

---
+++ uploadTextClient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  uploadTextClient
+//
+//  Created by ag20253 on 10/19/17.
+//  Copyright © 2017 ag20253. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
